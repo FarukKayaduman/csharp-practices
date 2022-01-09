155c9cd1f74ecaef4c0fa8e91f9c7a1e6c99b119
Faruk Kayaduman <farukkayaduman@outlook.com>
2022-01-09 15:49:44 +0300

    Update Program.cs

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -20,6 +20,9 @@ namespace DecisionStructures
             string message = time <= 18 ? "Have a nice day!" : "Good nights!";
             message = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have a nice day!" : "Good nights!";
             Console.WriteLine(message);
+
+            // Switch Case
+
         }
     }
 }
