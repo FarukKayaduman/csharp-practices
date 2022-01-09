b3eb371053565c0d68c466103b41a24ca091e967
Faruk Kayaduman <farukkayaduman@outlook.com>
2022-01-09 15:17:23 +0300

    Update

---
+++ IfElse-TernaryIf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfElse_TernaryIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11)
+                Console.WriteLine("Good morning!");
+            else if (time >= 11 && time <= 18)
+                Console.WriteLine("Have a nice day!");
+            else
+                Console.WriteLine("Good nights!");
+
+            // Ternary If
+
+            string message = time <= 18 ? "Have a nice day!" : "Good nights!";
+            message = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have a nice day!" : "Good nights!";
+            Console.WriteLine(message);
+        }
+    }
+}
