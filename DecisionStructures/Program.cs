fb6dd97155ac0bf7bebe53126a69913e321821b5
Faruk Kayaduman <farukkayaduman@outlook.com>
2022-01-09 15:45:15 +0300

    Update

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 
-namespace IfElse_TernaryIf
+namespace DecisionStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             int time = DateTime.Now.Hour;
 
-            if(time >= 6 && time < 11)
+            // If Else
+            if (time >= 6 && time < 11)
                 Console.WriteLine("Good morning!");
             else if (time >= 11 && time <= 18)
                 Console.WriteLine("Have a nice day!");
@@ -17,7 +17,6 @@ namespace IfElse_TernaryIf
                 Console.WriteLine("Good nights!");
 
             // Ternary If
-
             string message = time <= 18 ? "Have a nice day!" : "Good nights!";
             message = time >= 6 && time < 11 ? "Good morning!" : time <= 18 ? "Have a nice day!" : "Good nights!";
             Console.WriteLine(message);
