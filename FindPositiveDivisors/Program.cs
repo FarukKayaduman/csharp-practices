8a74b95bb89f778a9ce8c56e1fe2b90c0fab394e
Faruk Kayaduman <farukkayaduman@outlook.com>
2022-06-06 21:53:46 +0300

    Initial commit

---
+++ FindPositiveDivisors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindPositiveDivisors
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number; // The number to calculate divisors of.
+
+            Console.Write("Enter a number to calculate divisors: ");
+            number = Convert.ToInt32(Console.ReadLine()); // Get a number from user and assign to 'number' variable.
+
+            Console.Write("Divisors of " + number + ": 1 "); // '1' is a divisors for all numbers.
+
+            for(int i = 2; i <= number / 2; i++) // There is not any diviser between 'number/2' and 'number'. So no need to check all of them.
+            {
+                if (number % i == 0) // If the remainder of dividing the number by 'i' is equal to 0, means its a divisor.
+                    Console.Write( i + " " );
+            }
+
+            Console.WriteLine(number); // The number is a divisor of itself.
+        }
+    }
+}
